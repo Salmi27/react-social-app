5c7038295460e70150a80d11212972ae99b2f79a
Salmi27 <salmirumey27@gmail.com>
2024-01-22 00:17:20 +0530

    rightbar

--- social-media-app/src/components/rightbar/RightBar.jsx
+++ social-media-app/src/components/rightbar/RightBar.jsx
@@ -1,7 +1,147 @@
 import "./rightbar.scss";
+import dp from "../../assets/register-image.jpeg";
 
 const RightBar = () => {
-  return <h3>Rightbar</h3>;
+  return (
+    <div className="rightbar">
+      <div className="container">
+        <div className="items">
+          <span>Suggestion for you</span>
+          <div className="user">
+            <div className="userInfo">
+              <img src={dp} alt="" />
+              <span>John Doe</span>
+            </div>
+            <div className="buttons">
+              <button>follow</button>
+              <button>dismiss</button>
+            </div>
+          </div>
+
+          <div className="user">
+            <div className="userInfo">
+              <img src={dp} alt="" />
+              <span>John Doe</span>
+            </div>
+            <div className="buttons">
+              <button>follow</button>
+              <button>dismiss</button>
+            </div>
+          </div>
+        </div>
+
+        <div className="items">
+          <span>Latest Activities</span>
+          <div className="user">
+            <div className="userInfo">
+              <img src={dp} alt="" />
+              <span>John Doe</span>
+            </div>
+            <p>
+              <span>changed their cover picture</span> 1 min ago
+            </p>
+          </div>
+
+          <div className="user">
+            <div className="userInfo">
+              <img src={dp} alt="" />
+              <span>John Doe</span>
+            </div>
+            <p>
+              <span>changed their cover picture</span> 1 min ago
+            </p>
+          </div>
+
+          <div className="user">
+            <div className="userInfo">
+              <img src={dp} alt="" />
+              <span>John Doe</span>
+            </div>
+            <p>
+              <span>changed their cover picture</span> 1 min ago
+            </p>
+          </div>
+
+          <div className="user">
+            <div className="userInfo">
+              <img src={dp} alt="" />
+              <span>John Doe</span>
+            </div>
+            <p>
+              <span>changed their cover picture</span> 1 min ago
+            </p>
+          </div>
+        </div>
+
+        <div className="items">
+          <span>Online Friends</span>
+          <div className="user">
+            <div className="userInfo">
+              <img src={dp} alt="" />
+              <div className="online"/>
+              <span>John Doe</span>
+            </div>
+          </div>
+
+          <div className="user">
+            <div className="userInfo">
+              <img src={dp} alt="" />
+              <div className="online"/>
+              <span>John Doe</span>
+            </div>
+          </div>
+
+          <div className="user">
+            <div className="userInfo">
+              <img src={dp} alt="" />
+              <div className="online"/>
+              <span>John Doe</span>
+            </div>
+          </div>
+
+          <div className="user">
+            <div className="userInfo">
+              <img src={dp} alt="" />
+              <div className="online"/>
+              <span>John Doe</span>
+            </div>
+          </div>
+
+          <div className="user">
+            <div className="userInfo">
+              <img src={dp} alt="" />
+              <div className="online"/>
+              <span>John Doe</span>
+            </div>
+          </div>
+
+          <div className="user">
+            <div className="userInfo">
+              <img src={dp} alt="" />
+              <div className="online"/>
+              <span>John Doe</span>
+            </div>
+          </div>
+
+          <div className="user">
+            <div className="userInfo">
+              <img src={dp} alt="" />
+              <div className="online"/>
+              <span>John Doe</span>
+            </div>
+          </div>
+
+          <div className="user">
+            <div className="userInfo">
+              <img src={dp} alt="" />
+              <div className="online"/>
+              <span>John Doe</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default RightBar;
